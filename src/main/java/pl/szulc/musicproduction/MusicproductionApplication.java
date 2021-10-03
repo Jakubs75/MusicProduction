82c40301d74c1c395b020274ccc33abde2762e13
Jakub Szulc <kuba.szulc1@gmail.com>
2021-10-03 18:08:08 +0200

    initial commit

---
+++ src/main/java/pl/szulc/musicproduction/MusicproductionApplication.java
@@ -0,0 +1,13 @@
+package pl.szulc.musicproduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicproductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicproductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/szulc/musicproduction/MusicproductionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szulc.musicproduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicproductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
